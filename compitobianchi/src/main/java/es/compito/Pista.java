186838067649581674c91c261108a78cf2d11b89
lai2-24 <lai2-24@itismeucci.com>
2024-05-10 13:14:21 +0200

    compito

--- compitobianchi/src/main/java/es/compito/Pista.java
+++ compitobianchi/src/main/java/es/compito/Pista.java
@@ -4,11 +4,12 @@ public class Pista {
     private int persEntrate =4;
 
     public synchronized int corri(){
+        int tGiri=0;
         String nome = Thread.currentThread().getName();
 
         try {
             while(persEntrate==0){
-                System.out.println("Pista piena");
+                //System.out.println("Pista piena");
                 wait();
             }
         }
@@ -17,11 +18,14 @@ public class Pista {
         }
         persEntrate--;
         System.out.println(nome + " sale sul go-kart");
-        int t=1000 + (int) (Math.random()*2000);
+
         int giri = 1000 + (int)(Math.random()*5000);
-        int tGiri = (t/1000)*(giri/1000);
+        for(int i=0; i<giri/1000; i++){
+            int t=1000 + (int) (Math.random()*2000);
+            tGiri += (t/1000);
+        }
         System.out.println(nome + " il pilota occupa la pista per: " + tGiri + "s");
-        return t;
+        return tGiri;
     }
 
     public synchronized void esci(){
